f1a324e81f3a53e8fb71812c37c42acfd4e42055
haianpreuss <preusshaian@gmail.com>
2019-09-19 03:33:21 -0300

    Criada a classe "DAO" do "Endereco".

---
+++ src/java/br/edu/senai/model/dao/EnderecoDAO.java
@@ -0,0 +1,110 @@
+package br.edu.senai.model.dao;
+
+import br.edu.senai.connection.ConnectionFactory;
+import br.edu.senai.model.bean.Endereco;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class EnderecoDAO {
+    ConnectionFactory connection;
+
+    public EnderecoDAO() {
+        connection = new ConnectionFactory();
+    }
+
+    public void daoInsertEndereco(Endereco endereco) {
+        PreparedStatement stmt = null;
+
+        try {
+            connection.openConnection();
+            stmt = connection.getConection().prepareStatement("");
+            stmt.setString(1, endereco.getCepEndereco());
+            stmt.setString(2, endereco.getUfEndereco());
+            stmt.setString(3, endereco.getCidadeEndereco());
+            stmt.setString(4, endereco.getBairroEndereco());
+            stmt.setString(5, endereco.getLogradouroEndereco());
+            stmt.setString(6, endereco.getNumeroEndereco());
+            stmt.setString(7, endereco.getComplementoEndereco());
+            stmt.setString(8, endereco.getReferenciaEndereco());
+            stmt.executeUpdate();
+            connection.confirm();
+        } catch (SQLException ex) {
+            Logger.getLogger(EnderecoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            connection.closeConnection(stmt);
+        }
+    }
+
+    public List<Endereco> daoReadCompleteEnderecoList() {
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        List<Endereco> enderecos = new ArrayList<>();
+
+        try {
+            connection.openConnection();
+            stmt = connection.getConection().prepareStatement("");
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                Endereco endereco = new Endereco();
+                endereco.setIdEndereco(rs.getInt("idendereco"));
+                endereco.setCepEndereco(rs.getString("cep_endereco"));
+                endereco.setUfEndereco(rs.getString("uf_endereco"));
+                endereco.setCidadeEndereco(rs.getString("cidade_endereco"));
+                endereco.setBairroEndereco(rs.getString("bairro_endereco"));
+                endereco.setLogradouroEndereco(rs.getString("logradouro_endereco"));
+                endereco.setNumeroEndereco(rs.getString("numero_endereco"));
+                endereco.setComplementoEndereco(rs.getString("complemento_endereco"));
+                endereco.setReferenciaEndereco(rs.getString("referencia_endereco"));                
+                enderecos.add(endereco);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(EnderecoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            connection.closeConnection(stmt, rs);
+        }
+        return enderecos;
+    }
+
+    public void daoUpdateEndereco(Endereco endereco) {
+        PreparedStatement stmt = null;
+
+        try {
+            stmt = connection.getConection().prepareStatement("");
+            stmt.setInt(1, endereco.getIdEndereco());
+            stmt.setString(2, endereco.getCepEndereco());
+            stmt.setString(3, endereco.getCidadeEndereco());
+            stmt.setString(4, endereco.getBairroEndereco());
+            stmt.setString(5, endereco.getLogradouroEndereco());
+            stmt.setString(6, endereco.getNumeroEndereco());
+            stmt.setString(7, endereco.getComplementoEndereco());
+            stmt.setString(8, endereco.getReferenciaEndereco());
+            stmt.executeUpdate();
+            connection.confirm();
+        } catch (SQLException ex) {
+            Logger.getLogger(EnderecoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            connection.closeConnection(stmt);
+        }
+    }
+
+    public void daoDeleteEndereco(int id) {
+        PreparedStatement stmt = null;
+
+        try {
+            connection.openConnection();
+            stmt = connection.getConection().prepareStatement("");
+            stmt.setInt(1, id);
+            stmt.executeUpdate();
+            connection.confirm();
+        } catch (SQLException ex) {
+            Logger.getLogger(EnderecoDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            connection.closeConnection(stmt);
+        }
+    }
+}
