b239a3491902b2aa1ee9fc43f343a87dfbec2ef8
Haian Preuss <>
2019-09-19 22:19:03 -0300

    Criado classe de controle de "Saida"

---
+++ src/java/br/edu/senai/controller/SaidaController.java
@@ -0,0 +1,33 @@
+package br.edu.senai.controller;
+
+import br.edu.senai.model.bean.Saida;
+import br.edu.senai.model.dao.SaidaDAO;
+import java.util.List;
+
+public class SaidaController {
+    SaidaDAO saidaDao = new SaidaDAO();
+    
+    //INSERT
+    public boolean controllerInsertSaida(Saida saida){
+        //TODO requisitos
+        return saidaDao.daoInsertSaida(saida);
+    }
+    
+    //READ
+    public List<Saida> controllerReadCompleteSaida(){
+        //TODO requisitos
+        return null;
+    }
+    
+    //UPDATE
+    public boolean controllerUpdateSaida(Saida saida){
+        //TODO requisitos
+        return saidaDao.daoUpdateSaida(saida);
+    }
+    
+    //DELETE
+    public boolean controllerDeleteSaida(int id){
+        //TODO requisitos
+        return saidaDao.daoDeleteSaida(id);
+    }
+}
