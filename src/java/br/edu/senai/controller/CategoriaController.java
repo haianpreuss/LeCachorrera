bd1cb6b0bd1fbe7ba80cdd99c639846435ea13fb
Haian Preuss <>
2019-09-19 22:17:13 -0300

    Criado classe de controle de "Categoria"

---
+++ src/java/br/edu/senai/controller/CategoriaController.java
@@ -0,0 +1,33 @@
+package br.edu.senai.controller;
+
+import br.edu.senai.model.bean.Categoria;
+import br.edu.senai.model.dao.CategoriaDAO;
+import java.util.List;
+
+public class CategoriaController {
+    CategoriaDAO categoriaDao = new CategoriaDAO();
+    
+    //INSERT
+    public boolean controllerInsertCategoria(Categoria categoria){
+        //TODO requisitos
+        return categoriaDao.daoInsertCategoria(categoria);
+    }
+    
+    //READ
+    public List<Categoria> controllerReadCompleteCategoria(){
+        //TODO requisitos
+        return null;
+    }
+    
+    //UPDATE
+    public boolean controllerUpdateCategoria(Categoria categoria){
+        //TODO requisitos
+        return categoriaDao.daoUpdateCategoria(categoria);
+    }
+    
+    //DELETE
+    public boolean controllerDeleteCategoria(int id){
+        //TODO requisitos
+        return categoriaDao.daoDeleteCategoria(id);
+    }
+}
